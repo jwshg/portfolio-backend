c242ca6d0e86ad5af07b1ad324884d36301af208
jwshg <joao.galvao.pedro23@gmail.com>
2025-05-28 16:46:23 -0300

    Update server.js

--- backend-portfolio/src/server.js
+++ backend-portfolio/src/server.js
@@ -21,7 +21,10 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors({
+  origin: ['https://portfoliojg.com', 'http://localhost:3000'],
+  credentials: true
+} ));
 app.use(helmet());
 app.use(morgan('dev'));
 
